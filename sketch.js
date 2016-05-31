85c24816ce0b8a0939134c8e63e92780ee738936
emilyxxie <emilyxxie@gmail.com>
2016-05-31 16:13:54 -0400

    p5 scaffold for mini workshop at recurse

---
+++ sketch.js
@@ -0,0 +1,13 @@
+// you always need a setup function in p5.js
+// this is the initial setup that gets applied
+// before you begin drawing
+function setup() {
+
+}
+
+// you always need a draw function in p5.js
+// this is the function that gets called continuously in the background
+// you can think of this function as what gets applied in each frame
+function draw() {
+
+}
