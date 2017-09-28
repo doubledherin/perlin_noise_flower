c40a182761e3720aaa8dd617e65d50b401674924
Wendy Dherin <wendydherin@gmail.com>
2017-09-28 14:09:41 -0500

    Generate noise flower using perlin noise.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,35 @@
-/** 
-You always need a setup function in p5.js.
-This gets applied just once upon the initial
-load of your sketch.
-**/
-function setup() {
+// http://xie-emily.com/generative_art/noise_flower.html
 
+var centerX
+var centerY
+var radius
+var totalDegrees = 360
+
+function setup() {
+    createCanvas(
+        window.innerWidth,
+        window.innerHeight
+    )
+    background(0)
+    centerX = width / 2
+    centerY = height / 2
+    radius = height / 2
+    angleMode(DEGREES)
 }
 
-/** 
-You always need a draw function in p5.js.
-Right after setup(), gets executed, this gets called continuously 
-in the background. You can think of this function as 
-your primary animation loop––each "frame" of your sketch.
-**/
 function draw() {
-
+    noFill()
+    stroke(255, 128, 128, 25)
+    beginShape()
+        for (let i=0; i <=totalDegrees; i++) {
+            var noiseFactor = noise(i / 40, frameCount / 120)
+            var x = centerX + radius * cos(i) * noiseFactor
+            var y = centerY + radius * sin(i) * noiseFactor
+            curveVertex(x, y)
+        }
+    endShape(CLOSE)
+    if (radius <= 0) {
+        noLoop()
+    }
+    radius -= 0.65
 }
