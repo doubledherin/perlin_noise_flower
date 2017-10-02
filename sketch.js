6ee327c9f02c79a62174757ae536f9028a0a03df
Wendy Dherin <wendydherin@gmail.com>
2017-10-01 18:05:21 -0700

    Create a stem.

--- sketch.js
+++ sketch.js
@@ -2,37 +2,57 @@ var centerX
 var centerY
 var radius
 var totalDegrees = 369
+var direction
+var rotation
 var r = 255
 var g = 128
 var b = 128
 
 function setup() {
-    createCanvas(
+    canvas = createCanvas(
         window.innerWidth,
         window.innerHeight
     )
     background(0)
     centerX = width / 2
     centerY = height / 2
-    radius = height / 2
+    radius = 1
     angleMode(DEGREES)
     translateX = 0
     translateY = 0
     opacity = 255
+    direction = "out"
+    rotation = 24.0
 }
 
 function draw() {
+
     noFill()
     stroke(r, g, b, opacity)
     beginShape()
         for (let i=0; i <=totalDegrees; i++) {
-            var noiseFactor = noise(i / 40, frameCount / 120)
+            var noiseFactor = noise(i / 40, frameCount / 320)
             var x = centerX + radius * cos(i) * noiseFactor
             var y = centerY + radius * sin(i) * noiseFactor
             curveVertex(x, y)
+            rotate(PI/rotation);
         }
     endShape(CLOSE)
-    radius -= 0.65
+    if (direction == "out") {
+        if (radius > height / 2) {
+            direction == "in"
+        } else {
+            radius += 0.65
+        }
+    } else {
+        if (radius <= 0) {
+            radius = 100
+            direction == "out"
+        } else {
+            radius -= 0.65
+        }
+    }
+    rotation += 0.1
     if (frameCount > 200) {
         translateX = 0
         translateY = 0
